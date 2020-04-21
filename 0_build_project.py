db586ed88841cc8d3d47ef905ac765970a5a3cfe
Jeff Fletcher <fletch.jeff@gmail.com>
2020-04-21 15:10:52 +0200

    mkdir fix

--- 0_build_project.py
+++ 0_build_project.py
@@ -42,6 +42,8 @@ except:
   storage_environment = cml.create_environment_variable(storage_environment_params)
   os.environ["STORAGE"] = s3_bucket
 
+!hdfs dfs -mkdir -p $STORAGE/datalake/
+!hdfs dfs -mkdir -p $STORAGE/datalake/data/
 !hdfs dfs -mkdir -p $STORAGE/datalake/data/churn/
 !hdfs dfs -copyFromLocal /home/cdsw/raw/WA_Fn-UseC_-Telco-Customer-Churn-.csv $STORAGE/datalake/data/churn/WA_Fn-UseC_-Telco-Customer-Churn-.csv
 
