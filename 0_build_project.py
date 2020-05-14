afeb123bc100b67606bebbd7bf01a7d04ad74847
fletchjeff <jeff@DESKTOP-1H2CCBD.localdomain>
2020-05-14 10:09:19 +0200

    Reverting to non MLOps

--- 0_build_project.py
+++ 0_build_project.py
@@ -14,15 +14,6 @@ import requests
 import xml.etree.ElementTree as ET
 import datetime
 
-# Import SparkSQL for making tables for Atlas Integration
-from pyspark.sql import SparkSession
-from pyspark.sql.types import *
-spark = SparkSession\
-    .builder\
-    .appName("PythonSQL")\
-    .master("local[*]")\
-    .getOrCreate()
-
 run_time_suffix = datetime.datetime.now()
 run_time_suffix = run_time_suffix.strftime("%d%m%Y%H%M%S")
 
@@ -70,7 +61,7 @@ project_details = cml.get_project({})
 project_id = project_details["id"]
 
 
-# Create Training Job
+# Create Job
 create_jobs_params = {"name": "Train Model " + run_time_suffix,
                       "type": "manual",
                       "script": "3_train_models.py",
@@ -102,18 +93,19 @@ new_job_id = new_job["id"]
 print("Created new job with jobid", new_job_id)
 
 ##
-# Start the Training Job
+# Start a job
 job_env_params = {}
 start_job_params = {"environment": job_env_params}
 job_id = new_job_id
 job_status = cml.start_job(job_id, start_job_params)
-print("Training Job started")
+print("Job started")
 
 # Stop a job
 #job_dict = cml.start_job(job_id, start_job_params)
 #cml.stop_job(job_id, start_job_params)
 
 # Run experiment
+
 run_experiment_params = {
     "size": {
         "id": 1,
@@ -161,58 +153,6 @@ def run_experiment(params):
 default_engine_details = cml.get_default_engine({})
 default_engine_image_id = default_engine_details["id"]
 
-spark.sql("CREATE DATABASE IF NOT EXISTS telco_churn").show()
-
-create_table_statement = '''CREATE TABLE IF NOT EXISTS telco_churn.historical_data (
-  customerID STRING, 
-  gender STRING, 
-  SeniorCitizen STRING, 
-  Partner STRING, 
-  Dependents STRING, 
-  tenure DOUBLE, 
-  PhoneService STRING, 
-  MultipleLines STRING, 
-  InternetService STRING,
-  OnlineSecurity STRING, 
-  OnlineBackup STRING, 
-  DevbiceProtection STRING, 
-  TechSupport STRING, 
-  StreamingTV STRING, 
-  StreamingMovies STRING, 
-  Contract STRING, 
-  PaperlessBilling STRING, 
-  PaymentMethod STRING, 
-  MontlyCharges DOUBLE,
-  TotalCharges  DOUBLE,
-  Churn STRING)'''
-
-create_view_statement = '''CREATE TABLE IF NOT EXISTS telco_churn.training_data AS 
-SELECT customerID, 
-  SeniorCitizen, 
-  Partner, 
-  Dependents, 
-  tenure, 
-  PhoneService, 
-  MultipleLines, 
-  InternetService, 
-  OnlineSecurity, 
-  OnlineBackup, 
-  DevbiceProtection, 
-  TechSupport, 
-  StreamingTV, 
-  StreamingMovies, 
-  Contract, 
-  PaperlessBilling, 
-  PaymentMethod, 
-  MontlyCharges,
-  TotalCharges,
-  Churn
-FROM telco_churn.historical_data'''
-
-spark.sql(create_table_statement).show()
-# TODO: this often fails with permissions problems. Should be used to show more lineage. 
-# spark.sql(create_view_statement).show()
-
 # Create Model
 example_model_input = {"StreamingTV": "No", "MonthlyCharges": 70.35, "PhoneService": "No", "PaperlessBilling": "No", "Partner": "No", "OnlineBackup": "No", "gender": "Female", "Contract": "Month-to-month", "TotalCharges": 1397.475,
                        "StreamingMovies": "No", "DeviceProtection": "No", "PaymentMethod": "Bank transfer (automatic)", "tenure": 29, "Dependents": "No", "OnlineSecurity": "No", "MultipleLines": "No", "InternetService": "DSL", "SeniorCitizen": "No", "TechSupport": "No"}
@@ -238,19 +178,6 @@ create_model_params = {
     "replicationPolicy": {"type": "fixed", "numReplicas": 1},
     "environment": {}}
 
-# Complete our YAML file
-yaml_text = \
-""""Model Explainer {}":
-  hive_table_qualified_names:                # this is a predefined key to link to training data
-    - "telco_churn.historical_data@cm"         # the qualifiedName of the hive_table object representing                
-  metadata:                                  # this is a predefined key for additional metadata
-    query: "select * from historical_data"   # suggested use case: query used to extract training data
-    training_file: "3_train_models.py"       # suggested use case: training file used
-""".format(run_time_suffix)
-
-with open('lineage.yml', 'w') as lineage: lineage.write(yaml_text)
-
-# Deploy the model
 new_model_details = cml.create_model(create_model_params)
 access_key = new_model_details["accessKey"]  # todo check for bad response
 model_id = new_model_details["id"]
@@ -269,57 +196,7 @@ while is_deployed == False:
   else:
     print ("Deploying Model.....")
     time.sleep(10)
-
-# Set CRN for deployment
-crn_env_params = {"DEPLOYMENT_CRN":new_model_details["modelDeployment"]["crn"]}
-crn_env = cml.create_environment_variable(crn_env_params)
-
-# Create Accuracy Job
-create_calc_jobs_params = {"name": "Calculate Accuracy " + run_time_suffix,
-                      "type": "manual",
-                      "script": "6_calculate_accuracy.py",
-                      "timezone": "America/Los_Angeles",
-                      "environment": {},
-                      "kernel": "python3",
-                      "cpu": 1,
-                      "memory": 2,
-                      "nvidia_gpu": 0,
-                      "include_logs": True,
-                      "notifications": [
-                          {"user_id": user_obj["id"],
-                           "user":  user_obj,
-                           "success": False, "failure": False, "timeout": False, "stopped": False
-                           }
-                      ],
-                      "recipients": {},
-                      "attachments": [],
-                      "include_logs": True,
-                      "report_attachments": [],
-                      "success_recipients": [],
-                      "failure_recipients": [],
-                      "timeout_recipients": [],
-                      "stopped_recipients": []
-                      }
-
-new_calc_job = cml.create_job(create_calc_jobs_params)
-new_calc_job_id = new_calc_job["id"]
-print("Created new job with jobid", new_calc_job_id)
-
-time.sleep(10) # seems to blow up the first run most of the time
-
-# Start the accuracy job
-job_calc_env_params = {}
-start_job_calc_params = {"environment": job_env_params}
-job_calc_id = new_calc_job_id
-job_calc_status = cml.start_job(job_calc_id, start_job_calc_params)
-print("Accuracy Job 2 Started")
-
-# Do it again for two data points for the graph
-job_calc_env_params = {}
-start_job_calc_params = {"environment": job_env_params}
-job_calc_id = new_calc_job_id
-job_calc_status = cml.start_job(job_calc_id, start_job_calc_params)
-print("Accuracy Job 2 Started")
+    
 
 # Change the line in the flask/single_view.html file.
 import subprocess
--- 4_model_serve_explainer.py
+++ 4_model_serve_explainer.py
@@ -2,64 +2,23 @@ import os
 import sys
 from collections import ChainMap
 from pandas.io.json import dumps as jsonify
-import numpy
 
-sys.path.append("/home/cdsw") 
+#sys.path.append("/home/cdsw") 
 
 from churnexplainer.utils import log_environment
 from churnexplainer.explainedmodel import ExplainedModel
 
-#import new SDK
-import cdsw
-
 em = ExplainedModel(os.getenv('CHURN_MODEL_NAME', 'test_model'))
 
-@cdsw.model_metrics
 def explain(args):
+    #data = dict(ChainMap(request.args, em.default_data))
     data = dict(ChainMap(args, em.default_data))
     data = em.cast_dct(data)
-
-    #Do the prediction and provide weights for the reasoning
     probability, explanation = em.explain_dct(data)
-    
-    #NEW! Track our inputs
-    for key in data:
-      if isinstance(data[key], numpy.int64) or isinstance(data[key], numpy.float64):
-        cdsw.track_metric(key, data[key].item())
-      else:
-        cdsw.track_metric(key, data[key])
-    
-    #NEW! Track our prediction
-    cdsw.track_metric('probability', probability)
-    
-    #NEW! Track explanation
-    cdsw.track_metric('explanation', explanation)
-    
-    return {'data': dict(data),
+    return jsonify({'data': dict(data),
                     'probability': probability,
-                    'explanation': explanation}
+                    'explanation': explanation})
 
-# Preheat the model to get data into the metrics store - this runs at deployment
-s = [
-      {"StreamingTV":"No","MonthlyCharges":70.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Male","Contract":"Month-to-month","TotalCharges":1597.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":40.15,"PhoneService":"Yes","PaperlessBilling":"No","Partner":"No","OnlineBackup":"Yes","gender":"Female","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":75.35,"PhoneService":"Yes","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":1317.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":70.65,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":10.35,"PhoneService":"Yes","PaperlessBilling":"Yes","Partner":"Yes","OnlineBackup":"Yes","gender":"Male","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":40.25,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":1357.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":70.35,"PhoneService":"Yes","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":70.35,"PhoneService":"No","PaperlessBilling":"Yes","Partner":"No","OnlineBackup":"No","gender":"Male","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":70.32,"PhoneService":"Yes","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":417.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":70.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"Yes","gender":"Male","Contract":"Month-to-month","TotalCharges":1967.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":71.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"Yes","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":97.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":72.95,"PhoneService":"Yes","PaperlessBilling":"Yes","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":70.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Male","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":73.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Male","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":70.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"Yes","OnlineBackup":"Yes","gender":"Male","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":148.35,"PhoneService":"Yes","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":17.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":1.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"Yes","gender":"Female","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-      {"StreamingTV":"No","MonthlyCharges":79.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"},
-]  
-
-for x in s:
-  explain(x)
+#test
+#x={"StreamingTV":"No","MonthlyCharges":70.35,"PhoneService":"No","PaperlessBilling":"No","Partner":"No","OnlineBackup":"No","gender":"Female","Contract":"Month-to-month","TotalCharges":1397.475,"StreamingMovies":"No","DeviceProtection":"No","PaymentMethod":"Bank transfer (automatic)","tenure":29,"Dependents":"No","OnlineSecurity":"No","MultipleLines":"No","InternetService":"DSL","SeniorCitizen":"No","TechSupport":"No"}
+#explain(x)
